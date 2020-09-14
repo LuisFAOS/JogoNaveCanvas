e3208879ae7f4f2f2768881956cdafdacc4895b6
Luis Felipe <luisFAOSetecH@gmail.com>
2020-09-14 20:04:35 -0300

    adding sprite to the spaceship

--- src/elements/spaceship.js
+++ src/elements/spaceship.js
@@ -1,10 +1,13 @@
 let X_ARROW = [65,68]
 let Y_ARROW = [87,83] 
 
+import Spritesheet from '../features/spritesheet.js'
 import Shot from './shot.js'
 
 export default function Spaceship (context, image, keyboard, collider) {
     return {
+        id: 'spaceship',
+        spritesheet: new Spritesheet(context, image, 3, 2, 100),
         image,
         context,
         keyboard,
@@ -12,26 +15,37 @@ export default function Spaceship (context, image, keyboard, collider) {
         y:440,
         collider,
         update() {
-            const CanvasWidth=this.context.canvas.width+10
-            const CanvasHeight=this.context.canvas.height-20
-
-            if(keyboard.pressed(X_ARROW[0]) && this.x > 1){
-                this.x-=5
+            const increment = 300 * this.animation.time_between_circles / 1000;
+            if(keyboard.pressed(X_ARROW[0]) && 
+                this.x > 1){
+                this.x-=increment
             }
-            if(keyboard.pressed(X_ARROW[1]) && this.x < CanvasWidth - this.image.height){
-                this.x+=5
+            
+            if(keyboard.pressed(X_ARROW[1]) && 
+                this.x < this.context.canvas.width - 36){
+                this.x+=increment
             }
+            
             if(keyboard.pressed(Y_ARROW[0]) && this.y > 50){
-                this.y-=5
+                this.y-=increment
             }
-            if(keyboard.pressed(Y_ARROW[1]) && this.y < CanvasHeight - this.image.width){
-                this.y+=5
+            
+            if(keyboard.pressed(Y_ARROW[1]) && 
+                this.y < this.context.canvas.height - 48){
+                this.y+=increment
             }     
         
         },
         draw () {
-            const img = this.image
-            this.context.drawImage(img, this.x, this.y, img.width, img.height)
+        if (this.keyboard.pressed(65))
+            this.spritesheet.line = 1;
+         else if (this.keyboard.pressed(68))
+            this.spritesheet.line = 2;
+         else
+            this.spritesheet.line = 0;      
+         
+         this.spritesheet.draw(this.x, this.y);
+         this.spritesheet.next_frame();
         },
         shot(){
             const shot= new Shot(context,this)
@@ -45,19 +59,11 @@ export default function Spaceship (context, image, keyboard, collider) {
                 {x: this.x+13, y: this.y+3, width: 10, height: 33},
                 {x: this.x+25, y: this.y+19, width: 9, height: 13}
             ];
-            // Desenhando os retângulos para visualização
-            /* const ctx = this.context;
-            for (let i in rets) {
-                ctx.save();
-                ctx.strokeStyle = 'yellow';
-                ctx.strokeRect(rets[i].x, rets[i].y, rets[i].width, rets[i].height);
-                ctx.restore();
-            } */
                         
             return rets;  
         },
         collided_with(other){
-            if (other instanceof Object) {
+            if (other.id === 'ovni') {
                 this.animation.turnOff();
             }
         }
